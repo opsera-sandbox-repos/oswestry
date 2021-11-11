64759090df206023af6ad2ab1c04f82bbb354043
apat35 <93975209+apat35@users.noreply.github.com>
2021-11-11 17:14:26 +0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('port', 3010);
+
+app.get('/', (req, res) => {
+  res.send('Node API is up and running on port 3000...\n');
+});
+
+var server = app.listen(app.get('port'), function () {
+  var port = server.address().port;
+  console.log("api on port" + port);
+});
