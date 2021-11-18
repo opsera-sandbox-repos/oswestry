89db45bbbbc398147a90d8dc38226c86b04ed005
apat35 <sparuna@protonmail.com>
2021-11-18 18:00:42 +0400

    command line push

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-var express = require('express');
-var app = express();
-
-app.set('port', 3010);
-
-app.get('/', (req, res) => {
-  res.send('Node API is up and running on port 3000...\n');
-});
-
-var server = app.listen(app.get('port'), function () {
-  var port = server.address().port;
-  console.log("api on port" + port);
-});
-
 console.log("Testing the workflow part 1");
