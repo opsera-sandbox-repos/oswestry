9b60b9e62c8eb842ce911f87932a9c41e102bc43
apat35 <93975209+apat35@users.noreply.github.com>
2022-01-03 17:02:56 +0400

    bug fixes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Testing the workflow part 1");
 console.log("resetting deliveries");
 console.log("testing elk entries");
-console.log("testing elk entries once more);
+console.log("testing elk entries once more");
