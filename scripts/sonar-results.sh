fa784c4a0c811fed445867c33a837624c123336e
apat35 <93975209+apat35@users.noreply.github.com>
2022-01-27 14:03:26 +0400

    appending metadata

--- scripts/sonar-results.sh
+++ scripts/sonar-results.sh
@@ -4,3 +4,8 @@
 sonarResult=$(curl -v -H "Content-Type: application/json" -u "9b55f230d43dae27f6965acbb9ff622f70f3baed": "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots")
 echo "printing results..." 
 echo $sonarResult
+echo "appending metadata"
+metadata={"workflowId":18670092, "runId":1755477395, "applicationId":1231451}
+metadata >> sonarResult
+echo "printing results after appending metadata" 
+echo $sonarResult
