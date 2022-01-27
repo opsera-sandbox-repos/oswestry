970e5877bbab3c73b07f893eee11a1b7a3e5c625
apat35 <93975209+apat35@users.noreply.github.com>
2022-01-27 12:42:01 +0400

    changing get metrics

--- scripts/sonar-results.sh
+++ scripts/sonar-results.sh
@@ -5,4 +5,4 @@
 curl -X GET -header "Content-Type: application/json" \
         -header "Accept: application/json"  \
         -header "Authorization: Bearer fd88fe29c3526cdd8fe50ab1ad105d5c073f79c8"
-        "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots"
+        "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=bugs,vulnerabilities,security_hotspots"
