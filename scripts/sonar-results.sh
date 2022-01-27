13ce230596896f25cfb38334b81f1a0149447e67
apat35 <93975209+apat35@users.noreply.github.com>
2022-01-27 10:20:28 +0400

    bash script

--- scripts/sonar-results.sh
+++ scripts/sonar-results.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # This is a comment!
-curl --request GET -u "${{ secrets.SONARQUBE_PASS }}":"${{ secrets.SONARQUBE_HOST }}" --url ${{ secrets.SONARQUBE_HOST }}/api/measures/component?component=oswestry&metricKeys=bugs,vulnerabilities,security_hotspots
+curl -u "${{ secrets.SONARQUBE_PASS }}":"${{ secrets.SONARQUBE_HOST }}/api/measures/component?component=oswestry&metricKeys=bugs,vulnerabilities,security_hotspots"
+
