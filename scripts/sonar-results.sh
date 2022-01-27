74a2364781358eab5b15a1f28c91bd11cc61e829
apat35 <93975209+apat35@users.noreply.github.com>
2022-01-27 21:18:23 +0400

    dynamic runid

--- scripts/sonar-results.sh
+++ scripts/sonar-results.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "printing run ID "
+"run_id": $1
+echo $run_id
+
 sonarResult=$(curl -v -H "Content-Type: application/json" -u "9b55f230d43dae27f6965acbb9ff622f70f3baed": "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots")
 echo "printing results..." 
 echo $sonarResult
@@ -6,7 +10,8 @@ echo $sonarResult
 echo "appending metadata"
 
 # User can make a REST API call to Github to fetch the workflow metadata
-metadata="{"workflowId":"18670092", "runId":"1755477395", "applicationId":"1231451"}"
+metadata="{"run_id":$run_id}"
+# metadata="{"workflowId":"18670092", "runId":"1755477395", "applicationId":"1231451"}"
 echo $metadata
 
 sonarResult+=",{"metaData":$metadata}"
