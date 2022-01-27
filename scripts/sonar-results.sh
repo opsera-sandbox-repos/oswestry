23f4da12879b8717e91e92ffb87e335d943e9d36
apat35 <93975209+apat35@users.noreply.github.com>
2022-01-27 21:43:15 +0400

    atetqagakebnae

--- scripts/sonar-results.sh
+++ scripts/sonar-results.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "printing run ID"
 echo $1
+echo $run_id
 
 sonarResult=$(curl -v -H "Content-Type: application/json" -u "9b55f230d43dae27f6965acbb9ff622f70f3baed": "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots")
 echo "printing results..." 
