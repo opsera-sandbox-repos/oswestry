ba08b05599bac40d0e0c1bebd92f0c7612e930fd
apat35 <93975209+apat35@users.noreply.github.com>
2022-01-27 10:25:14 +0400

    url added

--- scripts/sonar-results.sh
+++ scripts/sonar-results.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This is a comment!
-curl -u "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots":"Op$era_20!9"
+curl -u "Op$era_20!9":"https://sqprod.opsera.io/" --url "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots"
 
