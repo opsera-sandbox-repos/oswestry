b2170cbb5feb7e347698be691b720ae3fd2a12ea
apat35 <93975209+apat35@users.noreply.github.com>
2022-01-27 13:11:33 +0400

    new line

--- scripts/sonar-results.sh
+++ scripts/sonar-results.sh
@@ -2,7 +2,9 @@
 # This is a comment!
 # curl -u "Op$era_20!9":"https://sqprod.opsera.io/" --url "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots"
 
-curl --request GET -header "Content-Type: application/json" \
-        -header "Accept: application/json"  \
-        -header "Authorization: Bearer fd88fe29c3526cdd8fe50ab1ad105d5c073f79c8"
-        -url "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=bugs,vulnerabilities,security_hotspots"
+# curl --request GET -header "Content-Type: application/json" \
+#         -header "Accept: application/json"  \
+#         -header "Authorization: Bearer fd88fe29c3526cdd8fe50ab1ad105d5c073f79c8"
+#         -url "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=bugs,vulnerabilities,security_hotspots"
+
+curl -v -H "Content-Type: application/json" -H "Authorization: Bearer fd88fe29c3526cdd8fe50ab1ad105d5c073f79c8" --request GET "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=bugs,vulnerabilities,security_hotspots"
