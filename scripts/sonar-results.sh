3a51d7013f3017aeb7e13c69592fa7e4dc048951
apat35 <93975209+apat35@users.noreply.github.com>
2022-01-27 12:38:25 +0400

    changing char

--- scripts/sonar-results.sh
+++ scripts/sonar-results.sh
@@ -3,6 +3,6 @@
 # curl -u "Op$era_20!9":"https://sqprod.opsera.io/" --url "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots"
 
 curl -X GET –header Content-Type: application/json’ \
-        –header ‘Accept: application/json’  \
-        –header ‘Authorization: Bearer fd88fe29c3526cdd8fe50ab1ad105d5c073f79c8’
+        –header "Accept: application/json"  \
+        –header "Authorization: Bearer fd88fe29c3526cdd8fe50ab1ad105d5c073f79c8"
         "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots"
