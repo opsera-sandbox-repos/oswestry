64ba9afe455c099dc5c03dbd5da2826e5ba256de
apat35 <93975209+apat35@users.noreply.github.com>
2022-01-27 16:14:58 +0400

    jobid

--- scripts/metadata.sh
+++ scripts/metadata.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-metadata=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/apat35/oswestry/actions/runs)
+# Step 1: To fetch the workflow runId
+workflow_metadata=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/apat35/oswestry/actions/runs)
 echo $metadata
+
+# Step 2: From Step 1, use the runId to request for the jobId & jobUrl
+job_metadata=$(curl \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/apat35/oswestry/actions/runs/1756074156/jobs 
+
+# Step 3: From Step 2, get the jobId & jobUrl 
+# Append the jobId, runId and workflowId to the sonar result after results are fetched 
