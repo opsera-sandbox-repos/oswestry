1a26b6a1ad016dcc605c5d3e9abf62efea219fb3
apat35 <93975209+apat35@users.noreply.github.com>
2022-01-27 13:38:46 +0400

    echo variable

--- scripts/sonar-results.sh
+++ scripts/sonar-results.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-# This is a comment!
-# curl -u "Op$era_20!9":"https://sqprod.opsera.io/" --url "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots"
 
-# curl --request GET -header "Content-Type: application/json" \
-#         -header "Accept: application/json"  \
-#         -header "Authorization: Bearer fd88fe29c3526cdd8fe50ab1ad105d5c073f79c8"
-#         -url "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=bugs,vulnerabilities,security_hotspots"
-
-# curl -v -H "Content-Type: application/json" -H "Authorization: Bearer 9b55f230d43dae27f6965acbb9ff622f70f3baed" --request GET "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots"
-curl -v -H "Content-Type: application/json" -u "9b55f230d43dae27f6965acbb9ff622f70f3baed": "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots" 
+# curl -v -H "Content-Type: application/json" -u "9b55f230d43dae27f6965acbb9ff622f70f3baed": "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots" 
+sonarResult=$(curl -v -H "Content-Type: application/json" -u "9b55f230d43dae27f6965acbb9ff622f70f3baed": "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots")
+echo "printing results..." 
+echo $sonarResult
