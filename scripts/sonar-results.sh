106a38e08c93952bf5d2e9b04cf29b7a158d8b4e
apat35 <93975209+apat35@users.noreply.github.com>
2022-01-27 12:43:42 +0400

    adding url

--- scripts/sonar-results.sh
+++ scripts/sonar-results.sh
@@ -2,7 +2,7 @@
 # This is a comment!
 # curl -u "Op$era_20!9":"https://sqprod.opsera.io/" --url "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=new_bugs,new_vulnerabilities,new_security_hotspots"
 
-curl -X GET -header "Content-Type: application/json" \
+curl --request GET -header "Content-Type: application/json" \
         -header "Accept: application/json"  \
         -header "Authorization: Bearer fd88fe29c3526cdd8fe50ab1ad105d5c073f79c8"
-        "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=bugs,vulnerabilities,security_hotspots"
+        --url "https://sqprod.opsera.io/api/measures/component?component=oswestry&metricKeys=bugs,vulnerabilities,security_hotspots"
