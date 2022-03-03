641196eecd0e95c70229ce28604bedc407fecca1
apat35 <sparuna@protonmail.com>
2022-03-04 00:19:33 +0400

    trying to fail a build

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-console.log("Testing the workflow part 1");
-console.log("resetting deliveries");
-console.log("testing elk entries");
-console.log("testing elk entries once more");
-console.log("testing with anand");
+var express = require('express');
+var app = express();
+
+app.set('port', 3000);
+
+app.get('/', (req, res) => {
+  res.send('Node API is up and running on port 3000...\n');
+});
+
+var server = app.listen(app.get('port'), function () {
+  var port = server.address().port;
+  console.log("api on port" + port)}
