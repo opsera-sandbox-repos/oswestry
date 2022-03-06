0ce782dd8e4885d7b741babff8efe71e7c78d2c7
apat35 <sparuna@protonmail.com>
2022-03-06 22:00:48 +0400

    fixing the bug

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
 console.log("test oswestry application success.. running on port 3010")
-var 23 = 23 ;
-
-console.log("test process failure .. exit and roll back to dev"
+console.log("test process failure .. exit and roll back to dev");
