7c926ce182e3a44805033458263303965eb076a1
spraruna <aruna@opsera.io>
2022-11-09 21:56:53 +0400

    p1 resolve

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("test oswestry application success.. running on port 3010")
 console.log("testing a new step")
 console.log("suriya asked me to trigger a pipeline")
+console.log("testing a new step")
