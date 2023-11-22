f963269306d5880bc69b97c227315160bfdcbcf8
Suriya Prakash <suriya.prakash@opsera.io>
2023-11-22 18:30:57 +0530

    DT-312 auto commit

--- autocommit.js
+++ autocommit.js
@@ -1,1 +1,20 @@
-// Trying a auto commit for generating github actions data.
+const scriptPath = './autocommit.sh';
+const { exec } = require('child_process');
+const runEvery5Mins = () => {
+    const random = Math.floor(Math.random() * (300 - 100) + 100);
+    const command = `bash ${scriptPath} ${new Date().toISOString()} ${random}`;
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
+setInterval(()=>{
+    runEvery5Mins();
+},60000)
---
+++ autocommit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Your file path
+file_path="test3.js"
+
+# Line to add
+new_line="// This is a new line added by the script. $1"
+
+# Add a line to the file
+echo "$new_line" >> "$file_path"
+
+# Git commands to stage and commit changes
+git add "$file_path"
+git commit -m "DT-$2 Automated Commit $1"
+git push
